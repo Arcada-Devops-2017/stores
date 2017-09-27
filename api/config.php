16c929eb7678997bb1b6ea427248d319f8730d43
Erik Nasaroff <eriknasaroff@gmail.com>
2017-09-27 15:36:37 +0300

    Changed MySql user

--- api/config.php
+++ api/config.php
@@ -4,7 +4,7 @@
 
 	# Edit these
 	define( 'DB_HOST', 'localhost' );
-	define( 'DB_USER', 'root');
+	define( 'DB_USER', 'stores');
 	define( 'DB_PASS', 'stores');
 	define( 'DB_NAME', 'stores');
 	define( 'DB_DRIVER', 'mysql');
