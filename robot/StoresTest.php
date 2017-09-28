9b7c42e47b95707b45026e3c5250af32576c3582
Emil <emil.nasaroff@gmail.com>
2017-09-28 16:50:58 +0300

    reverting changes

--- robot/StoresTest.php
+++ robot/StoresTest.php
@@ -15,13 +15,13 @@ class StoresTest extends PHPUnit_Framework_TestCase
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
-    // public function testStores()
-    // {
-    //     $API = new API($this->pdo);
-    //     $test = json_decode($API->stores(), TRUE);
+    public function testStores()
+    {
+        $API = new API($this->pdo);
+        $test = json_decode($API->stores(), TRUE);
 
-    //     $this->assertEquals(200, $test["status"]);
-    // }
+        $this->assertEquals(200, $test["status"]);
+    }
    
 }
 
--- robot/bootstrap.php
+++ robot/bootstrap.php
@@ -1,16 +1,11 @@
 <?php
 
-include_once('AutoLoader.php');
-// Register the directory to your include files
-AutoLoader::registerDirectory('../api');
-AutoLoader::registerDirectory('../');
+function loader($class)
+{
+    $file = $class . '.php';
+    if (file_exists($file)) {
+        require $file;
+    }
+}
 
-// function loader($class)
-// {
-//     $file = $class . '.php';
-//     if (file_exists($file)) {
-//         require $file;
-//     }
-// }
-
-// spl_autoload_register('loader');
+spl_autoload_register('loader');
