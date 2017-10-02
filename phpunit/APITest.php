20f2a860838a98522525d140c05df85634bea879
Emil Nasaroff <e.milnasaroff@gmail.com>
2017-10-02 20:36:20 +0300

    added more tests

--- phpunit/APITest.php
+++ phpunit/APITest.php
@@ -44,6 +44,9 @@ class APITest extends PHPUnit_Framework_TestCase
         $test = json_decode($API->stores('invalid_id'), TRUE);
         $this->assertEquals(400, $test['status']);
 
+        $test = json_decode($API->stores(3.33), TRUE);
+        $this->assertEquals(400, $test['status']);
+
     }
 
     public function testStores_with_product()
@@ -53,11 +56,35 @@ class APITest extends PHPUnit_Framework_TestCase
         $test = json_decode($API->stores_with_product(2), TRUE);
         $this->assertEquals(200, $test['status']);
 
+        $this->assertArrayHasKey('id', $test['stores'][0]);
+        $this->assertArrayHasKey('name', $test['stores'][0]);
+
         $test = json_decode($API->stores_with_product(), TRUE);
+        $this->assertEquals(400, $test['status']);
+
+        $test = json_decode($API->stores_with_product('invalid_id'), TRUE);
         $this->assertEquals(400, $test['status']);
 
         $test = json_decode($API->stores_with_product(3.33), TRUE);
         $this->assertEquals(400, $test['status']);
+
+    }
+
+    public function testStore_products()
+    {
+        $API = new API($this->pdo);
+
+        $test = json_decode($API->store_products(1), TRUE);
+        $this->assertEquals(200, $test['status']);
+
+        $test = json_decode($API->store_products('invalid_id'), TRUE);
+        $this->assertEquals(400, $test['status']);
+
+        $test = json_decode($API->store_products(3.33), TRUE);
+        $this->assertEquals(400, $test['status']);
+
+        $test = json_decode($API->store_products(10), TRUE);
+        $this->assertEquals(404, $test['status']);
 
 
     }
